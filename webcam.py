63c31b4b140b61c89755c2598f5c205960927c28
Sahajs04 <127437067+Sahajs04@users.noreply.github.com>
2023-09-16 22:17:06 -0400

    Add files via upload

--- webcam.py
+++ webcam.py
@@ -1,5 +1,10 @@
+from roboflow import Roboflow
 import cv2
+rf = Roboflow(api_key="sKsQzha8jrnm9Onvmp6p")
+project = rf.workspace().project("rock-paper-scissors-sxsw")
+model = project.version(11).model
 
+count = 0
 # Get the webcam
 cap = cv2.VideoCapture(0)
 
@@ -12,28 +17,41 @@ if not cap.isOpened():
 frame_width = int(cap.get(3))
 # Create a window to display the webcam feed
 cv2.namedWindow("Webcam")
+count = 0
+cap = cv2.VideoCapture(0)
 
-# Flag to indicate whether the user wants to quit
-quit_requested = False
+while(cap.isOpened()):
 
-# Start a loop to capture frames from the webcam
-while not quit_requested:
-    # Capture the frame
+    
+  # f.read() methods returns a tuple, first element is a bool 
+  # and the second is frame
     ret, frame = cap.read()
+    if ret == True:
+        # save frame as a “temporary” jpeg file
+        cv2.imwrite('temp.jpg', frame)
+        # run inference on “temporary” jpeg file (the frame)
+        predictions = model.predict('temp.jpg')
+        predictions_json = predictions.json()
+        # printing all detection results from the image
+        print(predictions_json)
 
-    # If the frame was not captured, break the loop
-    if not ret:
+        # accessing individual predicted boxes on each image
+        for bounding_box in predictions:
+            # x0 = bounding_box['x'] - bounding_box['width'] / 2#start_column
+            # x1 = bounding_box['x'] + bounding_box['width'] / 2#end_column
+            # y0 = bounding_box['y'] - bounding_box['height'] / 2#start row
+            # y1 = bounding_box['y'] + bounding_box['height'] / 2#end_row
+            class_name = bounding_box['class']
+            confidence_score = bounding_box['confidence']
+        
+            detection_results = bounding_box
+            class_and_confidence = (class_name, confidence_score)
+            print(class_and_confidence, '\n')
+          
+
+    if cv2.waitKey(1000) == ord('q'):
         break
 
-    # Display the frame
-    cv2.imshow("Webcam", frame)
 
-    # Check for user input
-    key = cv2.waitKey(10)
-
-    # If the key is 'q', set the quit flag to True
-    if key == ord('q'):
-        quit_requested = True
-
-# Release the webcam
 cap.release()
+cv2.destroyAllWindows()
